dda675ffd6e9ce2be04ba3aa61e6cfc3ae20b517
sachinkumar9671549834-cloud <sachinkumar9671549834@gmail.com>
2026-07-03 15:35:33 +0530

    script.js
    Added functionality for play button and smooth scroll.

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// GeoMaster Website
 
+console.log("GeoMaster Loaded");
+
+const playBtn = document.querySelector(".start-btn");
+
+if (playBtn) {
+  playBtn.addEventListener("click", function(e) {
+    e.preventDefault();
+    alert("🚀 World Map Quiz is coming soon!");
+  });
+}
+
+// Smooth scroll for future use
+document.querySelectorAll("a[href='#']").forEach(link => {
+  link.addEventListener("click", function(e) {
+    e.preventDefault();
+  });
+});
